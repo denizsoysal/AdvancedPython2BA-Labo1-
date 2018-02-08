631ed6c09c06ece96197388bdd7753541fc5a78b
denizsoysal <15030@ecam.be>
2018-02-08 10:18:37 +0100

    good version of utils

--- utils.py
+++ utils.py
@@ -2,22 +2,27 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
-
+import math
 def fact(n):
-    """Computes the factorial of a natural number.
-    
-    Pre: -
-    Post: Returns the factorial of 'n'.
-    Throws: ValueError if n < 0
-    """
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
 
 def roots(a, b, c):
-    """Computes the roots of the ax^2 + bx + x = 0 polynomial.
-    
-    Pre: -
-    Post: Returns a tuple with zero, one or two elements corresponding
-          to the roots of the ax^2 + bx + c polynomial.
-    """
+
+
+    d = b ** 2 - 4 * a * c  # discriminant
+
+    if d < 0:
+        return none
+    elif d == 0:
+        x = (-b + math.sqrt(b ** 2 - 4 * a * c)) / 2 * a
+        return x
+    else:
+        x1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / 2 * a
+        x2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / 2 * a
+        return (x1,x2)
     pass
 
 def integrate(function, lower, upper):
